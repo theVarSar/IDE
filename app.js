87930daa07581e601a74d7e6a394cc394028b7bc
VarSar <varunsarpal271@gmail.com>
2019-08-02 17:24:40 +0530

    Add procfile

--- app.js
+++ app.js
@@ -73,6 +73,12 @@ setTimeout(function () {
   });
 });
 
-app.listen("3000", function(){
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server Started:3000");
 });
